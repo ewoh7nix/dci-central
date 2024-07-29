b34a1e5d6b83df87dbd41791f2ac18825e7d51c8
Nasrullah <ewoh7nix@gmail.com>
2024-07-29 15:27:47 +0700

    Added Terraform module to provision Azure Database for Flexible MySQL

---
+++ terraform/modules/azure_mysql/main.tf
@@ -0,0 +1,135 @@
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = var.resource_group_name_prefix
+}
+
+# Generate random value for the login password
+resource "random_password" "password" {
+  length           = 8
+  lower            = true
+  min_lower        = 1
+  min_numeric      = 1
+  min_special      = 1
+  min_upper        = 1
+  numeric          = true
+  override_special = "_"
+  special          = true
+  upper            = true
+}
+
+# Manages the Virtual Network
+resource "azurerm_virtual_network" "default" {
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  name                = "vnet-${var.resource_group_name_prefix}"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Manages the Subnet
+resource "azurerm_subnet" "datastore" {
+  address_prefixes     = ["10.0.2.0/24"]
+  name                 = "datastore"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  service_endpoints    = ["Microsoft.Storage"]
+
+  delegation {
+    name = "fs"
+
+    service_delegation {
+      name = "Microsoft.DBforMySQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+resource "azurerm_subnet" "appservices" {
+  address_prefixes     = ["10.0.3.0/24"]
+  name                 = "appservices"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  service_endpoints    = ["Microsoft.Storage"]
+
+  delegation {
+    name = "fs"
+
+    service_delegation {
+      name = "Microsoft.Web/serverFarms"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+# VNET integration app service
+#resource "azurerm_app_service_virtual_network_swift_connection" "dci_vnet_integration" {
+#  app_service_id = app_service.azurerm_linux_web_app.webapp.id
+#  subnet_id      = azurerm_subnet.appservices.id
+#}
+
+# Private endpoint
+#resource "azurerm_private_endpoint" "dcicentral" {
+#  name                = "example-endpoint"
+#  location            = azurerm_resource_group.rg.location
+#  resource_group_name = azurerm_resource_group.rg.name
+#  subnet_id           = azurerm_subnet.appservices.id
+#
+#  private_service_connection {
+#    name                           = "example-privateserviceconnection"
+#    private_connection_resource_id = azurerm_storage_account.example.id
+#    subresource_names              = ["blob"]
+#    is_manual_connection           = false
+#  }
+#
+#  private_dns_zone_group {
+#    name                 = "example-dns-zone-group"
+#    private_dns_zone_ids = [azurerm_private_dns_zone.example.id]
+#  }
+#}
+
+# Enables you to manage Private DNS zones within Azure DNS
+resource "azurerm_private_dns_zone" "default" {
+  name                = "${var.resource_group_name_prefix}.mysql.database.azure.com"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Enables you to manage Private DNS zone Virtual Network Links
+resource "azurerm_private_dns_zone_virtual_network_link" "default" {
+  name                  = "mysqlfsVnetZone${var.resource_group_name_prefix}.com"
+  private_dns_zone_name = azurerm_private_dns_zone.default.name
+  resource_group_name   = azurerm_resource_group.rg.name
+  virtual_network_id    = azurerm_virtual_network.default.id
+
+  depends_on = [azurerm_subnet.datastore]
+}
+
+# Manages the MySQL Flexible Server
+resource "azurerm_mysql_flexible_server" "default" {
+  location                     = azurerm_resource_group.rg.location
+  name                         = "mysqlfs-${var.resource_group_name_prefix}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  administrator_login          = replace(var.resource_group_name_prefix, "-", "_")
+  administrator_password       = random_password.password.result
+  backup_retention_days        = 7
+  delegated_subnet_id          = azurerm_subnet.datastore.id
+  geo_redundant_backup_enabled = false
+  private_dns_zone_id          = azurerm_private_dns_zone.default.id
+  sku_name                     = "B_Standard_B1s"
+  version                      = "8.0.21"
+  zone                         = 1
+
+  maintenance_window {
+    day_of_week  = 0
+    start_hour   = 8
+    start_minute = 0
+  }
+  storage {
+    iops    = 360
+    size_gb = 20
+  }
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.default]
+}
---
+++ terraform/modules/azure_mysql/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "dci-central-technical-test"
+  description = "Prefix of the resource group name"
+}
